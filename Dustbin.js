14c7eb497163abde64730709ddaeea9e4445b2c6
KishaBhatia <64302865+KishaBhatia@users.noreply.github.com>
2020-05-17 12:24:44 +0530

    Add files via upload

---
+++ Dustbin.js
@@ -0,0 +1,38 @@
+class Dustbin{
+
+    constructor(x,y,width,height){
+
+        var options={
+
+            isStatic:true,
+            
+        }
+
+        this.width=width;
+        this.height=height;
+        this.body1=Bodies.rectangle(x,y,width,height,options);
+        World.add(world,this.body1);
+        this.body2=Bodies.rectangle(x-80,y-40,20,100,options);
+        World.add(world,this.body2);
+        this.body3=Bodies.rectangle(x+80,y-40,20,100,options);
+        World.add(world,this.body3);
+    }
+
+
+    display(){
+
+        var pos1=this.body1.position
+        var pos2=this.body2.position
+        var pos3=this.body3.position
+        rectMode(CENTER);
+        push();
+        translate(pos1.x,pos1.y);
+        fill("green");
+        noStroke();
+        rect(0,0,this.width,this.height)
+        rect(-80,-40,20,100);
+        rect(80,-40,20,100);
+        pop();
+
+    }
+}
---
+++ Paper.js
@@ -0,0 +1,33 @@
+class Paper{
+
+    constructor(x,y){
+
+        var options={
+
+            isStatic:false,
+            restitution:0.3,
+            friction:0.5,
+            density:1.2
+        }
+
+    this.body=Bodies.circle(x,y,18,options);
+    this.x=x;
+    this.y=y;
+
+    World.add(world, this.body);
+    }
+
+
+    display(){
+
+        var pos =this.body.position;
+        push();
+        translate(pos.x,pos.y);
+        ellipseMode(CENTER);
+        fill("yellowe");
+        ellipse(0, 0,40,40);
+        pop();
+      
+    }
+    
+}
